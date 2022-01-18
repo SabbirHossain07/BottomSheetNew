750514eb2ae36551f41e381c611c80f9620246ed
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-01-18 19:14:56 +0600

    Initial Commit

---
+++ BottomSheetNew/BottomSheetNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetNewApp.swift
+//  BottomSheetNew
+//
+//  Created by Sopnil Sohan on 18/1/22.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
